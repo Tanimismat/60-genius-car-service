5926d9f85648c71791157f34f4012f14d3fbde4a
Tanimismat <tanim.ismat@gmail.com>
2022-04-19 05:09:51 +0600

    dynamic route set

--- src/Pages/Home/SingleService/SingleService.js
+++ src/Pages/Home/SingleService/SingleService.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SingleService = ({ service }) => {
-    const { name, img, price, description } = service;
+    const { id, name, img, price, description } = service;
+
+    const navigate = useNavigate()
+    const navigateToServiceDetail = (id) => {
+        navigate(`/service/${id}`);
+    }
     return (
         <div className='singleService'>
             <img className='w-100' src={img} alt="Car services" />
             <h2>single service {name}</h2>
             <p>Price: ${price}</p>
             <p><small>{description}</small></p>
-            <button>Book: {name}</button>
+            <button onClick={() => navigateToServiceDetail(id)}>Book: {name}</button>
         </div>
     );
 };
